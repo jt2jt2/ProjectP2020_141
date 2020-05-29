258e33b029e6c1f4bbcee58d3d2081c9cc01eefd
jt2jt2 <azusaorenoyome1111@gmail.com>
2020-05-29 14:16:04 +0900

    reset

---
+++ php/php3.php
@@ -0,0 +1,82 @@
+<!DOCTYPE html> 
+<html lang="ja">
+    <head>
+	<title>php3</title>
+    </head>
+    <body>    
+        <?php
+            $addresses = [
+                ["name" => "東京太郎","address" => "東京都", "phone" => "012-345-6789", "email" =>"taro@example.com"],
+                ["name" =>"工科花子","address" =>  "北海道","phone" => "987-654-3210","email" => "hana@example.com"]
+                ];
+                $plusaddress;
+                $plusaddress = ["name"=>$_POST['name'],"address" => $_POST['address'], "phone" => $_POST['phone'], "email" =>$_POST['email']];
+                //header('Location:php3.php');
+               // header('content-type: application/json; charset=utf-8');
+               $hoge=0;
+               if($_POST!=NULL){
+                    //$addresses[2]= $plusaddress;
+                     $addressJSON = json_encode($addresses,JSON_UNESCAPED_UNICODE | JSON_PRETTY_PRINT);
+                     foreach($addresses as $value){
+                        print $value["name"];
+                        print $value["address"];
+                        print $value["phone"];
+                        print $value["email"];
+                    }
+                }
+
+                var_dump($addressJSON);
+            function print_table($data_array){
+                print "<table border=1>";
+                print "<tr>";
+                print "<td>";
+                print "名前";
+                print "</td>";
+                print "<td>";
+                print "住所";
+                print "</td>";
+                print "<td>";
+                print "電話";
+                print "</td>";
+                print "<td>";
+                print "Email";
+                print "</td>";
+                print "</tr>";
+                foreach($data_array as $value){
+                    print "<tr>";
+                    print "<td>";
+                    print $value["name"];
+                    print "</td>";
+                    print "<td>";
+                    print $value["address"];
+                    print "</td>";
+                    print "<td>";
+                    print $value["phone"];
+                    print "</td>";
+                    print "<td>";
+                    print $value["email"];
+                    print "</td>";
+                    print "</tr>";
+                }
+                print"</table>";
+            }
+          //  if($addresses!=""){
+            //    post_print_table($addresses,$plusaddress);
+            //}else{
+
+                print_table($addresses);
+            //}
+        ?>
+    <form method="post" action="php3.php">
+    名前
+    <input type="text" name="name">
+    住所
+    <input type="text" name="address">
+    電話
+    <input type="text" name="phone">
+    Email
+    <input type="text" name="email">
+    <input type="submit" value="送信">
+    </form>
+    </body>
+</html>
