d5f1cd59902cc1dc96519712d1a77321643ab10d
jt2jt2 <azusaorenoyome1111@gmail.com>
2020-05-29 14:23:42 +0900

    charage_1

---
+++ php/php1.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html> 
+<html lang="ja">
+    <head>
+	<title>php1</title>
+    </head>
+    <body>
+        <?php
+            function printhello(){
+				print("HelloWorld!!");
+			}
+			printhello();
+        ?>
+    </body>
+</html>
