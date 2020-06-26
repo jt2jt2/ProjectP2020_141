4c5fac39ba66c74b4b6b55b4f7b37d7656a47014
jt2jt2 <azusaorenoyome1111@gmail.com>
2020-06-27 01:53:16 +0900

    js5 add

--- js/js5_server.php
+++ js/js5_server.php
@@ -1,8 +1,7 @@
 <?php
-    $message =  $_GET['message'];
-    $message = htmlspecialchars($message, ENT_QUOTES, 'UTF-8');
-    print($message."が送られました。");
-    $file = './addresses.json';
-    $json = file_get_contents($file); 
-    var_dump($json);
+    if($_SERVER['REQUEST_METHOD'] == 'GET') {
+        $message =  $_GET['message'];
+        $message = htmlspecialchars($message, ENT_QUOTES, 'UTF-8');
+        print($message."が送られました。");
+    }
 ?>
